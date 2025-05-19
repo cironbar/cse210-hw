b469f2bf8f491ab249181f2bbaf74b56ee02fc3c
cironbar <cironbar@byupathway.edu>
2025-05-19 14:13:58 +0100

    Add Job and Resume classes with display functionality

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+      Job job1 = new Job();
+        job1._jobTitle = "Software Developer";
+        job1._companyName = "Netflex";
+        job1._startDate = "12/05/2020";
+        job1._endDate = "11/011/2022";
+       
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Developer";
+        job2._companyName = "Google";
+        job2._startDate = "21/03/2022";
+        job2._endDate = "03/06/2024";
+        
+
+        Resume myResume = new Resume();
+        myResume._name = "Charles Ironbar";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
